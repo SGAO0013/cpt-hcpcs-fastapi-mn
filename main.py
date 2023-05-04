88ca14d511c87387bddb020d377b88a76b38c405
SGAO0013 <sherry.gao@stonybrook.edu>
2023-05-04 04:43:36 +0000

    this is a working version

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+import pandas as pd
+import uvicorn
+
+df = pd.read_csv('./data/services2019.csv')
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home():
+    return {"this is a API service for MN CPT code details"}
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/cpt/<value>', methods=['GET'])
+def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['svc_code'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.route('/icd/<value>/sex/<value2>')
+def icdcode2(value, value2):
+    filtered = df[df['svc_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")  
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
